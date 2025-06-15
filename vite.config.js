907d309e4a780a14c30c82549fe73f03b8b3209c
Adebayo Temiloluwa <temiloluwaa980@example.com>
2025-06-15 11:48:27 +0100

    Olorfare

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ExpenseTracker/', // 👈 Use your GitHub repo name here
 })
