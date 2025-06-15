467e7f9bf6c46d796aff1fd931dc47d01f7531af
Adebayo Temiloluwa <temiloluwaa980@example.com>
2025-06-15 13:29:43 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Expense-Tracker/', // must match GitHub repo name exactly (case-sensitive)
   plugins: [react()],
-  base: '/ExpenseTracker/', // 👈 Use your GitHub repo name here
-})
+});
